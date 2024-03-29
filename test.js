d461bba6670cbfe77bc2b58a7b1fe258b3cf4d61
WxylkxyZz <75302552+WxylkxyZz@users.noreply.github.com>
2024-03-29 15:33:37 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,3 @@
- /***
-  [task_local]
-  event-interaction https://raw.githubusercontent.com/KOP-XIAO/QuantumultX/master/Scripts/geo_location.js, tag=GeoIP 查询, img-url=location.fill.viewfinder.system
-  
-  @XIAO_KOP
-
-  **/
-
 // var content= `<p style="text-align: center; font-family: -apple-system; font-size: large; font-weight: bold">` + response.body + `</p>`;
 
   var url = "https://api.ip.sb/geoip"
